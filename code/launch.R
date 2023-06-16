0947258daff921ed62966a304a7e068d42e18faf
Gyorgy Barabas <dysordys@umich.edu>
2023-06-16 16:55:25 +0200

    Cluster launch code written.

---
+++ code/launch.R
@@ -0,0 +1,25 @@
+perms <- 1e2L
+
+for (r in 1:60) {
+  jobname <- sprintf("CP%02d.sh", r)
+  f <- file(jobname)
+  writeLines(c(
+    paste("#!/bin/bash"),
+    paste("#SBATCH -J", jobname),
+    paste("#SBATCH -A liu-2018-29"),
+    paste("#SBATCH -t 24:00:00"),
+    paste("#SBATCH --mem=16000"),
+    paste("#SBATCH -n 1"),
+    paste("# "),
+    paste("# Run single task in foreground"),
+    paste("module add R/3.5.1-nsc1-gcc-2018a"),
+    paste("cd", Sys.getenv("PWD")),
+    paste("Rscript code/generate-perm-table-cluster.R", rownum, perms),
+    paste("# "),
+    paste("# Script ends here")
+  ), f)
+  close(f)
+  system(paste("sbatch", jobname))
+  Sys.sleep(0.1)
+}
+
