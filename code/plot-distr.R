4de20fa934d2932351b7fcc3ce3ef24c68cfc28d
Gyorgy Barabas <dysordys@gmail.com>
2023-06-18 11:37:45 +0200

    Generalized distribution plotting script (code/plot-distr.R).

--- code/plot-distr.R
+++ code/plot-distr.R
@@ -1,11 +1,17 @@
 library(tidyverse)
 
+top1 <- max
+
+top10 <- function(x) mean(tail(sort(x), 10))
+
+obsDiff <- function(w, m, metric) match.fun(metric)(w) - match.fun(metric)(m)
+
 read_csv("data/permdat/master.csv", show_col_types = FALSE) %>%
   filter(!juniors, !inactives, floor == 1400, metric == "top1") %>%
   transmute(data = map(file, read_rds)) %>%
   unnest(data) %>%
   filter(fed == "ARG") %>%
-  mutate(obs = map2_dbl(`F`, `M`, ~max(.x) - max(.y))) %>%
+  mutate(obs = pmap_dbl(list(`F`, `M`, metric), obsDiff)) %>%
   select(obs, permuts) %>%
   unnest(permuts) %>%
   ggplot(aes(x = permuts)) +
