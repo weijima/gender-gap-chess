eb2b5086748fb3f3cc5a687707a21049f7cc16d3
Gyorgy Barabas <dysordys@gmail.com>
2023-07-27 19:11:03 +0200

    Small tweak to new figure code

--- code/rating-diff.R
+++ code/rating-diff.R
@@ -7,15 +7,19 @@ read_csv("data/null-stats.csv", show_col_types = FALSE) %>%
             by = join_by(metric, juniors, inactives, floor, fed)) %>%
   select(-c(stat, E, A, weight)) %>%
   filter(metric %in% c("mean", "top1", "top10")) %>%
-  mutate(filter = str_c("J", 1*juniors, "-I", 1*inactives), .before = 1) %>%
+  mutate(metric = fct_relevel(metric, "mean", "top10")) %>%
+  mutate(filter = fct_rev(str_c("J", 1*juniors, "-I", 1*inactives)), .before = 1) %>%
   arrange(juniors, inactives, floor, fed, metric) %>%
   pivot_longer(cols = starts_with("y"), names_to = "response", values_to = "gap") %>%
+  mutate(response = case_match(response, "y" ~ "raw", "yP" ~ "participation-corrected",
+                               "yPEA" ~ "PEA-corrected")) %>%
+  mutate(response = fct_relevel(response, "raw", "participation-corrected")) %>%
   ggplot(aes(x = as_factor(floor), y = gap, fill = filter)) +
-  geom_boxplot() +
+  geom_boxplot(alpha = 0.8, outlier.alpha = 0.3) +
   labs(x = "rating floor", y = "rating gap (men - women)") +
-  facet_grid(response ~ metric) +
+  facet_grid(metric ~ response, scale = "free_y") +
   scale_fill_manual(name = "filter: ",
-                    values = c("steelblue", "goldenrod", "forestgreen", "plum3")) +
+                    values = rev(c("steelblue","forestgreen","goldenrod","plum3"))) +
   theme_bw() +
   theme(legend.position = "bottom")
-#ggsave("figures/fig_4_test.pdf", width = 7, height = 6)
+#ggsave("figures/fig_4.pdf", width = 7, height = 6)
